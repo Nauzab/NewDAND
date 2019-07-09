c9c3881c004d9a95c83a82633b362b5606ad7865
DarijaSok <darija.sokolova@gmail.com>
2019-07-09 08:33:55 +0300

    commit

---
+++ src/main/java/com/example/demo/repo/ManagerRepo.java
@@ -0,0 +1,10 @@
+package com.example.demo.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.Manager;
+
+public interface ManagerRepo extends CrudRepository<Manager> {
+	
+
+}
